64e6c4f05e08406aeea81a516bb9e56a712aa358
Jessica Soltero <jmichellevs@gmail.com>
2017-03-29 13:39:55 -0700

    save user in database and return accessToken

--- model/user.js
+++ model/user.js
@@ -4,13 +4,14 @@ let mongoose = require('mongoose')
 
 let userSchema = mongoose.Schema({
   googleID: {type: String, required: true, unique: true},
-  refreshToken: String,
-  accessToken: String,
   name: {type: String, required: true, unique: true},
   email: {type: String, required: true},
   tasks: [{type: mongoose.Schema.Types.ObjectId, ref: 'tasks'}],
   profilePic: String,
-
+  refreshToken: String,
+  accessToken: String,
+  tokenTTL: Number,
+  tokenTimestamp: Date,
 })
 
 module.exports = mongoose.model('users', userSchema)
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,12 +2,12 @@
 
 const Router = require('express').Router
 const googleOAUTH = require('../lib/google-oauth-middleware')
-// const passport = require('passport')
+const User = require('../model/user.js')
 
 const router = module.exports = new Router()
 
 router.get('/auth/google', (req, res) => {
-  console.log('AT /AUTH/GOOGLE')
+  console.log('MEOW /AUTH/GOOGLE')
   const googleAuthBase = 'https://accounts.google.com/o/oauth2/v2/auth'
   const googleAuthResponseType = 'response_type=code'
   const googleAuthClientID = `client_id=${process.env.CLIENT_ID}`
@@ -22,72 +22,29 @@ router.get('/auth/google', (req, res) => {
 })
 
 router.get('/auth/google/callback', googleOAUTH, (req, res) => {
-  console.log('AT /auth/google/callback')
-  // console.log('REQ.GOOGLEOAUTH', req.googleOAUTH)
+  console.log('MEOW /auth/google/callback')
 
-  res.end()
-  // should have either req.googleError or req.googleOAUTH
-  // console.log('googleError', req.googleError);
-  // console.log('googleOAUTH', req.googleOAUTH);
-  //
-  // // if googleError deal with google Error
-  // if(req.googleError){
-  //   return res.redirect('/');
-  // }
-  //
-  // // check if user allreay exists
-  // User.findOne({email: req.googleOAUTH.email})
-  // .then(user => {
-  //   if (!user) return Promise.reject(new Error('user not found'));
-  //   return user;
-  // })
-  // .catch(err => {
-  //   if (err.message === 'user not found'){
-  //     let userData = {
-  //       username: req.googleOAUTH.email,
-  //       email: req.googleOAUTH.email,
-  //       google: {
-  //         googleID: req.googleOAUTH.googleID,
-  //         tokenTTL: req.googleOAUTH.tokenTTL,
-  //         tokenTimestamp: Date.now(),
-  //         refreshToken: req.googleOAUTH.refreshToken,
-  //         accessToken: req.googleOAUTH.accessToken,
-  //       }
-  //     }
-  //     return new User(userData).save()
-  //   }
-  //   return Promise.reject(err);
-  // })
-  // .then(user => user.generateToken())
-  // .then(token => {
-  //   res.redirect(`/?token=${token}`);
-  // })
-  // .catch(err => {
-  //   console.error(err);
-  //   console.log('user not found');
-  //   res.redirect('/');
-  // })
-  //res.send('lulwat');
-  //return new User(userData).save();
-  //})
-  //.then(user => user.generateToken())
-  //.then(token => {
-  //res.send(token);
-  //})
-  //.catch((err) => {
-  //console.error(err);
-  //res.send('boo hoo')
-  //});
+  // if googleError deal with google Error
+  if(req.googleError){
+    return res.redirect('/')
+  }
 
+  User.findOne({email: req.googleOAUTH.email})
+  .then(user => {
+    if (!user) {
+      let userData = {
+        googleID: req.googleOAUTH.googleID,
+        name: req.googleOAUTH.name,
+        email: req.googleOAUTH.email,
+        profilePic: req.googleOAUTH.profilePic,
+        accessToken: req.googleOAUTH.accessToken,
+        refreshToken: req.googleOAUTH.refreshToken,
+        tokenTTL: req.googleOAUTH.tokenTTL,
+        tokenTimestamp: Date.now()
+      }
+      user = new User(userData).save()
+    }
+    // probably shouldnt be returning the users accessToken?
+    res.json(user.accessToken)
+  })
 })
-
-
-
-
-
-//
-// (req, res) => {
-//   console.log('WE ARE IN AUTH GOOGLE/CALLBACK')
-//   console.log('USER TO TOTOTOTO', req.user)
-//   res.redirect('https://wattle.io')
-// })
