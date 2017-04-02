d3090aa188c0a148d6d799c1013844b4e772d322
Kyle Winckler <kyle.winckler@gmail.com>
2017-04-02 14:21:01 -0700

    create tag routes

--- routes/tag-routes.js
+++ routes/tag-routes.js
@@ -1,20 +1,33 @@
 'use strict'
 
-const Router = require('express').Router()
-const jsonParser = require('body-parser').json()
-const User = require('../model/user')
-const Task = require('../model/tag')
 const Tag = require('../model/tag')
+const bearerAuth = require('../lib/bearer-auth-middleware')
 
+const Router = require('express').Router
 const tagRouter = module.exports = new Router()
 
-tagRouter.post('/tags', jsonParser, (req, res, next) => {
-  Task.findById(req.params.id)
-    .then(task => {
-      return new Tag(req.body).save()
-    })
-    .then(tag => {
-      
-    })
+tagRouter.post('/api/tags', bearerAuth, (req, res, next) => {
+  req.body.user = req.user._id
 
+  new Tag(req.body).save()
+  .then(tag => res.json(tag))
+  .catch(next)
+})
+
+tagRouter.get('/api/tags', bearerAuth, (req, res, next) => {
+  Tag.find({user: req.user._id})
+  .then(tags => res.json(tags))
+  .catch(next)
+})
+
+tagRouter.put('/api/tags/:id', bearerAuth, (req, res, next) => {
+  Tag.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(tag => res.json(tag))
+  .catch(next)
+})
+
+tagRouter.delete('/api/tags/:id', bearerAuth, (req, res, next) => {
+  Tag.findByIdAndRemove(req.params.id)
+  .then(() => res.status(204).end())
+  .catch(next)
 })
