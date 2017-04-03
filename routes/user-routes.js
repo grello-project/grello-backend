6c26d72398c2d1343b6841830eaa3343e5f7e7d6
Jessica Soltero <jmichellevs@gmail.com>
2017-04-03 12:36:37 -0700

    Add delete user route

---
+++ routes/user-routes.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const User = require('../model/user.js')
+const Task = require('../model/task.js')
+const Category = require('../model/category.js')
+const Tag = require('../model/tag.js')
+const Document = require('../model/document.js')
+const bearerAuth = require('../lib/bearer-auth-middleware')
+
+const Router = require('express').Router
+const router = module.exports = new Router()
+
+router.delete('/api/user', bearerAuth, (req, res, next) => {
+  User.findByIdAndRemove(req.user._id)
+  .then(() => Task.remove({userID: req.user._id}))
+  .then(() => Category.remove({user: req.user._id}))
+  .then(() => Tag.remove({user: req.user._id}))
+  .then(() => Document.remove({user: req.user._id}))
+  .then(() => res.status(204).end())
+  .catch(next)
+})
