40c9c7457c501321c0e4fe40412fc8130e18f15c
Kyle Winckler <kyle.winckler@gmail.com>
2017-04-03 15:26:24 -0700

    test category POST route

---
+++ test/test-category.js
@@ -0,0 +1,89 @@
+'use strict'
+
+const app = require('../index')
+const request = require('superagent')
+const expect = require('chai').expect
+
+const User = require('../model/user')
+const Category = require('../model/category')
+
+const PORT = process.env.PORT || 3000
+
+process.env.MONGODB_URI = 'mongodb://localhost/devFinal'
+
+const url = 'http://localhost:3000'
+
+const mockUser = {
+  googleID: 'testGID',
+  name: 'test user',
+  email: 'test@test.com'
+}
+
+const mockCategory = {
+  name: 'test category'
+}
+
+describe('Testing Category Routes', function() {
+  let server
+
+  beforeEach(done => {
+
+    server = app.listen(PORT)
+
+    new User(mockUser).save()
+      .then(user => {
+        this.tempUser = user
+        return new Category(mockCategory).save()
+      })
+      .then(category => {
+        category.user = this.tempUser._id
+        return category.save()
+      })
+      .then(category => {
+        this.tempCategory = category
+        return this.tempUser.save()
+      })
+      .then(user => user.generateToken())
+      .then(token => {
+        this.tempUser.token = token
+        done()
+      })
+      .catch(done)
+  })
+
+  afterEach(done => {
+    User.remove({})
+    .then(() => Category.remove({}))
+    .then(() => {
+      server.close()
+      done()
+    })
+    .catch(done)
+  })
+
+  describe('POST', () => {
+
+    it('should create a new category', done => {
+      request
+      .post(`${url}/api/categories`)
+      .set('Authorization', `Bearer ${this.tempUser.token}`)
+      .send(mockCategory)
+      .end((err, res)=> {
+        expect(res.status).to.equal(200)
+        expect(res.body.name).to.equal(this.tempCategory.name)
+        expect(res.body.user).to.equal(this.tempUser._id.toString())
+        done()
+      })
+    })
+    it('should respond 401 if no Auth header', done => {
+      request
+        .post(`${url}/api/categories`)
+        .send(mockCategory)
+        .end((err, res) => {
+          expect(res.status).to.equal(401)
+          expect(res.text).to.equal('UnauthorizedError')
+          done()
+        })
+    })
+  })
+})
