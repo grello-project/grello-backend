fda44f39e23cb7fdd0563b25f1fea0a4226528d7
Jessica Soltero <jmichellevs@gmail.com>
2017-04-06 16:36:06 -0700

    trying to get fileID

--- model/task.js
+++ model/task.js
@@ -6,14 +6,14 @@ let taskSchema = mongoose.Schema({
   googleID: {type: String, unique: true},
   priority: Number,
   tag: {type: mongoose.Schema.Types.ObjectId, ref: 'tags'},
-  document: {type: mongoose.Schema.ObjectId, ref: 'documents'},
+  doc: {type: mongoose.Schema.ObjectId, ref: 'documents'},
   author: {type: String, required: true},
   category: {type: mongoose.Schema.Types.ObjectId, ref: 'categories'},
   userID: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
   comment: {type: String, required: true},
   dueDate: Date,
   assignedDate: Date,
-  done: Boolean
+  resolved: Boolean
 })
 
 module.exports = mongoose.model('tasks', taskSchema)
