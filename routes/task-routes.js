3fb03da2b234146d51cb159bc180dc40c6f88771
Jessica Soltero <jmichellevs@gmail.com>
2017-04-07 16:18:20 -0700

    the resolve property appears to be non-writable?!

--- routes/task-routes.js
+++ routes/task-routes.js
@@ -1,7 +1,10 @@
 'use strict'
 
+const request = require('superagent')
+
 const Task = require('../model/task.js')
 const bearerAuth = require('../lib/bearer-auth-middleware')
+const refreshToken = require('../lib/refresh-token-middleware')
 
 const Router = require('express').Router
 const router = module.exports = new Router()
@@ -14,36 +17,30 @@ router.get('/api/tasks', bearerAuth, (req, res, next) => {
   .catch(next)
 })
 
-router.put('/api/tasks/:id', bearerAuth, (req, res, next) => {
-
-// To refresh an access token, your application sends an HTTPS POST request
-// to Google's authorization server
-// (https://www.googleapis.com/oauth2/v4/token) that includes the following
-// parameters:
-// refresh_token The refresh token returned from the authorization code exchange.
-// client_id
-// client_secret
-// grant_type this field must contain a value of refresh_token.
-// SAMPLE request
-// POST /oauth2/v4/token HTTP/1.1
-// Host: www.googleapis.com
-// Content-Type: application/x-www-form-urlencoded
-//
-// client_id=<your_client_id>&
-// client_secret=<your_client_secret>&
-// refresh_token=<refresh_token>&
-// grant_type=refresh_token
-
-// SAMPLE response
-// {
-//   "access_token":"1/fFAGRNJru1FTz70BzhT3Zg",
-//   "expires_in":3920,
-//   "token_type":"Bearer"
-// }
-
-// PATCH / PUT https://www.googleapis.com/drive/v3/files/fileId/comments/commentId
+router.put('/api/tasks/:id', bearerAuth, refreshToken, (req, res, next) => {
+  console.log('IN THE API/TASK/ID ROUTE')
+  req.body.resolved = true
 
   Task.findByIdAndUpdate(req.params.id, req.body, {new: true})
-  .then(task => res.json(task))
+  .then(task => {
+    console.log('ADD CONTENT TO REQ.BODY', req.body)
+    req.body.content = task.comment
+
+    request.patch(`https://www.googleapis.com/drive/v3/files/${task.document}/comments/${task.googleID}?fields=author%2Ccontent%2Cdeleted%2ChtmlContent%2Cid%2CquotedFileContent%2Cresolved`)
+    .set('Authorization', `Bearer ${req.user.accessToken}`)
+    .send(req.body)
+    .then(resp => {
+      console.log('HERE IS OUR UPDATED COMMENT', resp.body)
+      res.json(task)
+    })
+    .catch(err => {
+      console.log('ERR FROM PATCHING', err)
+      res.end()
+    })
+    // .catch(next)
+
+    // res.json(task)
+
+  })
   .catch(next)
 })
