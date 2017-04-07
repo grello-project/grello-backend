b643adc5bd3b46049b3ea749b61a2fbc34ec4f70
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-04-06 18:59:09 -0700

    store doc googleID on task

--- model/task.js
+++ model/task.js
@@ -6,7 +6,7 @@ let taskSchema = mongoose.Schema({
   googleID: {type: String, unique: true},
   priority: Number,
   tag: {type: mongoose.Schema.Types.ObjectId, ref: 'tags'},
-  doc: {type: mongoose.Schema.ObjectId, ref: 'documents'},
+  document: {type: String, ref: 'documents'},
   author: {type: String, required: true},
   quote: String,
   category: {type: mongoose.Schema.Types.ObjectId, ref: 'categories'},
