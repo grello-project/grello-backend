b5511febaf085475c07fef4a6c59928feda0e56e
Dan Peters <daniel.r.peters@icloud.com>
2017-04-08 13:02:33 -0700

    fixed duplicate key bug and ignore log files

--- model/document.js
+++ model/document.js
@@ -3,7 +3,7 @@
 let mongoose = require('mongoose')
 
 let documentSchema = mongoose.Schema({
-  googleID: {type: String, required: true, unique: true},
+  googleID: {type: String, required: true},
   name: {type: String, required: true},
   user: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
   createdTime: Date,
