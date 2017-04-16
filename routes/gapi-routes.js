da16ea9fd80208e5b41e03154eb0ae47795fff4f
Dan Peters <daniel.r.peters@icloud.com>
2017-04-15 23:29:22 -0700

    re-wrote user authenticate and save functionality

--- routes/gapi-routes.js
+++ routes/gapi-routes.js
@@ -1,19 +1,17 @@
 'use strict'
 
 const Router = require('express').Router
+const router = module.exports = new Router()
 
 const FRONTEND_URL = process.env.WATTLE_URL || 'http://localhost:8080'
 // const BACKEND_URL = process.env.API_URL || 'http://localhost:3000'
 
-const google = require('googleapis')
-const plus = google.plus('v1')
-
 // const getFiles = require('../lib/files.js')
-const getFiles = require('../lib/gapi-promisifiedFilesList')
-const getTasks = require('../lib/gapi-promisifiedAndFilteredCommentsList')
-const User = require('../model/user.js')
+const authenticateAndSaveUser = require('../lib/authenticateAndSaveUser')
+const getFiles = require('../lib/getFiles')
+const getTasks = require('../lib/getTasks')
 
-let oauth2Client = require('../lib/gapi-OAuth2')
+let oauth2Client = require('../lib/oauth2Client')
 
 let scopes = [
   'openid',
@@ -27,7 +25,6 @@ let url = oauth2Client.generateAuthUrl({
   scope: scopes,
 })
 
-const router = module.exports = new Router()
 
 router.get('/gapi/auth', (req, res) => {
   res.redirect(url)
@@ -35,51 +32,25 @@ router.get('/gapi/auth', (req, res) => {
 
 router.get('/gapi/auth/success', (req, res) => {
   console.log('entered success route')
-  oauth2Client.getToken(req.query.code, (err, tokens) => {
-    if (err) return console.error(err)
-    oauth2Client.setCredentials(tokens)
-    // TODO: Package this into another module
-    plus.people.get({userId: 'me', auth: oauth2Client}, (err, profile) => {
-      let existingUser
-      if (err) console.error(err)
-      User.findOne({googleID: profile.id})
-        .then(user => {
-          if (!user) {
-            let userData = {
-              googleID: profile.id,
-              name: profile.displayName,
-              email: profile.emails[0].value,
-              profilePic: profile.image.url,
-              accessToken: tokens.access_token,
-              refreshToken: tokens.refresh_token,
-              expiration: tokens.expiry_date,
-            }
-            user = new User(userData).save()
-          } else {
-            existingUser = true
-          }
-          return Promise.resolve(user)
-        })
-        .then(user => {
-          if (existingUser) return Promise.resolve(user)
-          console.log('getting files')
-          return getFiles(user)
-        })
-        .then(filesResults => {
-          console.log('Files results:\n\n', filesResults.files[0], '\n\n')
-          console.log('getting comments')
-          return Promise.all(filesResults.files.slice(0,3).map(file => {
-            return getTasks(file, filesResults.user)
-          }))
-        })
-        .then(commentsResults => {
-          console.log('here are the comments results:\n\n', commentsResults, '\n\n')
-          return Promise.resolve(commentsResults[0].user)
-        })
-        // TODO: SAVE FILES HERE THEN generateToken
-        .then(user => user.generateToken())
-        .then(token => res.redirect(`${FRONTEND_URL}/#!/join?token=${token}`))
-        .catch(err => Promise.reject(err))
+  authenticateAndSaveUser(req.query.code)
+    .then(user => {
+      // if (existingUser) return Promise.resolve(user)
+      console.log('getting files')
+      return getFiles(user)
     })
-  })
+    .then(filesResults => {
+      console.log('Files results:\n\n', filesResults.files[0], '\n\n')
+      console.log('getting comments')
+      return Promise.all(filesResults.files.slice(0,3).map(file => {
+        return getTasks(file, filesResults.user)
+      }))
+    })
+    .then(commentsResults => {
+      console.log('here are the comments results:\n\n', commentsResults, '\n\n')
+      return Promise.resolve(commentsResults[0].user)
+    })
+    // TODO: SAVE FILES HERE THEN generateToken
+    .then(user => user.generateToken())
+    .then(token => res.redirect(`${FRONTEND_URL}/#!/join?token=${token}`))
+    .catch(err => console.error(err))
 })
