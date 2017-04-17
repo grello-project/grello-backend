d4cb59a721fbf7afb8f2c53066892d6739255459
Dan Peters <daniel.r.peters@icloud.com>
2017-04-16 17:20:59 -0700

    more work on new google api library

--- routes/gapi-routes.js
+++ routes/gapi-routes.js
@@ -34,22 +34,29 @@ router.get('/gapi/auth/success', (req, res) => {
   console.log('entered success route')
   authenticateAndSaveUser(req.query.code)
     .then(user => {
-      // if (existingUser) return Promise.resolve(user)
-      console.log('getting files')
-      return getFiles(user)
+      if (user.timesLoggedIn > 0) {
+        console.log('user already exists, skipping file/task saving functions')
+        return Promise.resolve(user)
+      }
+      else {
+        console.log('getting files')
+        // TODO: SET WATCH DRIVE HERE
+        return getFiles(user)
+        .then(filesResults => {
+          console.log('Files results:\n\n', filesResults.files[0], '\n\n')
+          console.log('getting comments')
+          // TODO: get rid of this slice before going live!!!!
+          return Promise.all(filesResults.files.slice(0,3).map(file => {
+            return getTasks(file, user)
+          }))
+        })
+        .then(commentsResults => {
+          console.log('here are the comments results:\n\n', commentsResults, '\n\n')
+          return Promise.resolve(commentsResults[0].user)
+        })
+        // TODO: SAVE FILES HERE THEN generateToken
+      }
     })
-    .then(filesResults => {
-      console.log('Files results:\n\n', filesResults.files[0], '\n\n')
-      console.log('getting comments')
-      return Promise.all(filesResults.files.slice(0,3).map(file => {
-        return getTasks(file, filesResults.user)
-      }))
-    })
-    .then(commentsResults => {
-      console.log('here are the comments results:\n\n', commentsResults, '\n\n')
-      return Promise.resolve(commentsResults[0].user)
-    })
-    // TODO: SAVE FILES HERE THEN generateToken
     .then(user => user.generateToken())
     .then(token => res.redirect(`${FRONTEND_URL}/#!/join?token=${token}`))
     .catch(err => console.error(err))
