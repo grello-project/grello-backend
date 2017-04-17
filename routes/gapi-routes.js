57147d1c3630c68d9a8b6030446d222cfc44da9b
Dan Peters <daniel.r.peters@icloud.com>
2017-04-17 14:19:52 -0700

    files.js is refactored

--- routes/gapi-routes.js
+++ routes/gapi-routes.js
@@ -10,6 +10,7 @@ const FRONTEND_URL = process.env.WATTLE_URL || 'http://localhost:8080'
 const authenticateAndSaveUser = require('../lib/authenticateAndSaveUser')
 const getFiles = require('../lib/getFiles')
 const getTasks = require('../lib/getTasks')
+const batchSave = require('../lib/batchSaveFilesAndTasks')
 
 let oauth2Client = require('../lib/oauth2Client')
 
@@ -31,33 +32,26 @@ router.get('/gapi/auth', (req, res) => {
 })
 
 router.get('/gapi/auth/success', (req, res) => {
-  console.log('entered success route')
   authenticateAndSaveUser(req.query.code)
-    .then(user => {
-      if (user.timesLoggedIn > 0) {
-        console.log('user already exists, skipping file/task saving functions')
-        return Promise.resolve(user)
-      }
-      else {
-        console.log('getting files')
-        // TODO: SET WATCH DRIVE HERE
-        return getFiles(user)
+  .then(user => {
+    if (user.timesLoggedIn > 0) {
+      return Promise.resolve(user)
+    }
+    else {
+      // TODO: SET WATCH DRIVE HERE
+      return getFiles(user)
         .then(filesResults => {
-          console.log('Files results:\n\n', filesResults.files[0], '\n\n')
-          console.log('getting comments')
           // TODO: get rid of this slice before going live!!!!
           return Promise.all(filesResults.files.slice(0,3).map(file => {
             return getTasks(file, user)
           }))
         })
         .then(commentsResults => {
-          console.log('here are the comments results:\n\n', commentsResults, '\n\n')
-          return Promise.resolve(commentsResults[0].user)
+          return batchSave(commentsResults, user)
         })
-        // TODO: SAVE FILES HERE THEN generateToken
-      }
-    })
-    .then(user => user.generateToken())
-    .then(token => res.redirect(`${FRONTEND_URL}/#!/join?token=${token}`))
-    .catch(err => console.error(err))
+    }
+  })
+  .then(user => user.generateToken())
+  .then(token => res.redirect(`${FRONTEND_URL}/#!/join?token=${token}`))
+  .catch(err => console.error(err))
 })
